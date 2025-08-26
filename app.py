ae23dede788f1a2c56284918284321ff744296ec
Kapil Dev <kdev7830.com>
2025-08-26 20:27:39 +0530

    Fix: Corrected MAIL_PASSWORD configuration

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'kdev7830@gmail.com'  # Replace with your email
-app.config['MAIL_PASSWORD'] = os.environ.get('kmwzakuuchugabaf')   # Replace with your password or app password
+app.config['MAIL_PASSWORD'] = 'kmwzakuuchugabaf'  # Replace with your password or app password
 
 mysql = MySQL(app)
 mail = Mail(app)
